1521a35f5137cd2ca7494a86d522877d2799853e
coord.e <me@coord-e.com>
2020-05-03 08:41:39 +0000

    Add: Enhance CLI

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,48 @@
+use std::fs::File;
 use std::io::{self, Write};
+use std::path::{Path, PathBuf};
 
 use satat::cnf::CNF;
+use satat::dimacs;
 use satat::solver::{self, solve};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "saturn")]
+struct Opt {
+    #[structopt(short, long, parse(from_occurrences))]
+    verbose: u8,
+
+    #[structopt(short = "f", long, parse(from_os_str), conflicts_with = "cnf")]
+    cnf_file: Option<PathBuf>,
+
+    #[structopt(short, long, conflicts_with = "cnf_file")]
+    cnf: Option<String>,
+}
+
+fn solve_cnf(cnf_string: impl AsRef<str>) -> Result<(), Box<dyn std::error::Error>> {
+    let cnf: CNF = cnf_string.as_ref().trim().parse()?;
+    if let Some(model) = solve::<solver::DPLL>(cnf) {
+        println!("SAT {}", model);
+    } else {
+        println!("UNSAT");
+    }
+    Ok(())
+}
+
+fn solve_cnf_file(cnf_file: impl AsRef<Path>) -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open(cnf_file)?;
+    let cnf = dimacs::parse(file)?;
+    if let Some(model) = solve::<solver::DPLL>(cnf) {
+        println!("SAT {}", model);
+    } else {
+        println!("UNSAT");
+    }
+    Ok(())
+}
+
+fn interactive() -> Result<(), Box<dyn std::error::Error>> {
     let mut buf = String::new();
     let mut stdout = io::stdout();
 
@@ -20,5 +59,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         } else {
             println!("UNSAT");
         }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let opt = Opt::from_args();
+
+    match (opt.cnf, opt.cnf_file) {
+        (Some(_), Some(_)) => unreachable!(),
+        (Some(cnf), _) => solve_cnf(cnf),
+        (_, Some(path)) => solve_cnf_file(path),
+        (None, None) => interactive(),
     }
 }
